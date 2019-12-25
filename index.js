06e1b8555ab5dc1e3f705b22d22860b3774e2703
benjachogit <40907304+benjachogit@users.noreply.github.com>
2019-12-25 16:16:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,35 +13,18 @@ const bodyParser = require('body-parser')
 
   app = express(),
   port = process.env.PORT || 1500;
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 
-
-const issue2options = {
-  origin: true,
-  methods: ["POST"],
-  credentials: true,
-  maxAge: 3600
-};
-
+app.use(cors())
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
 
-app.get('/', cors(issue2options) ,getCrop.getCropIMG)
+app.get('/', getCrop.getCropIMG)
 
-app.post('/signup', cors(issue2options) ,SCB.signup)
-app.post('/blob', cors(issue2options) , singleFileUpload.single('image'),blob.imageUpload)
-app.get('/getpromo', cors(issue2options) ,shoyuupro.selectpro)
-app.get('/getcustomer', cors(issue2options) ,customer.getCustomer)
+app.post('/signup', SCB.signup)
+app.post('/blob', singleFileUpload.single('image'),blob.imageUpload)
+app.get('/getpromo',shoyuupro.selectpro)
+app.get('/getcustomer',customer.getCustomer)
